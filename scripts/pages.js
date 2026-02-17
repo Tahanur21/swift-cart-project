14a728a52b2d94e782e2544e56b073de45741f80
Tahanur Islam <tahanur373@gmail.com>
2026-02-17 20:46:48 +0600

    adding-new-handler

--- scripts/pages.js
+++ scripts/pages.js
@@ -1,27 +1,37 @@
-const homeBtn = document.getElementById('home-btn');
-const productsBtn = document.getElementById('products-btn');
-const homeContent = document.getElementById('home-content');
-const productsContent = document.getElementById('products-content');
+const homeBtn = document.getElementById("home-btn");
+const productsBtn = document.getElementById("products-btn");
+const homeDropBtn = document.getElementById("home-drop-btn");
+const productsDropBtn = document.getElementById("products-drop-btn");
+
+const homeContent = document.getElementById("home-content");
+const productsContent = document.getElementById("products-content");
 
 const removeActive = () => {
-  homeBtn.classList.remove('active-link');
-  productsBtn.classList.remove('active-link');
+  homeBtn.classList.remove("active-link");
+  productsBtn.classList.remove("active-link");
+  homeDropBtn.classList.remove("active-link");
+  productsDropBtn.classList.remove("active-link");
 };
 
-homeBtn.addEventListener('click', () => {
+const showHome = () => {
   removeActive();
-  homeBtn.classList.add('active-link');
-  homeContent.classList.remove('hidden');
-  productsContent.classList.add('hidden');
-});
+  homeBtn.classList.add("active-link");
+  homeDropBtn.classList.add("active-link");
+  homeContent.classList.remove("hidden");
+  productsContent.classList.add("hidden");
+};
 
-productsBtn.addEventListener('click', () => {
+const showProducts = () => {
   removeActive();
-  productsBtn.classList.add('active-link');
-  productsContent.classList.remove('hidden');
-  homeContent.classList.add('hidden');
-});
+  productsBtn.classList.add("active-link");
+  productsDropBtn.classList.add("active-link");
+  productsContent.classList.remove("hidden");
+  homeContent.classList.add("hidden");
+};
 
-homeBtn.classList.add('active-link');
-homeContent.classList.remove('hidden');
-productsContent.classList.add('hidden');
+homeBtn.addEventListener("click", showHome);
+homeDropBtn.addEventListener("click", showHome);
+productsBtn.addEventListener("click", showProducts);
+productsDropBtn.addEventListener("click", showProducts);
+
+showHome();
