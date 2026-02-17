a8074803d565cecefc73e108c8001123e2217321
Tahanur Islam <tahanur373@gmail.com>
2026-02-17 20:38:33 +0600

    adding event handler

---
+++ scripts/dropPages.js
@@ -0,0 +1,27 @@
+const homeBtn = document.getElementById('home-drop-btn');
+const productsBtn = document.getElementById('products-drop-btn');
+const homeContent = document.getElementById('home-content');
+const productsContent = document.getElementById('products-content');
+
+const removeActive = () => {
+  homeBtn.classList.remove('active-link');
+  productsBtn.classList.remove('active-link');
+};
+
+homeBtn.addEventListener('click', () => {
+  removeActive();
+  homeBtn.classList.add('active-link');
+  homeContent.classList.remove('hidden');
+  productsContent.classList.add('hidden');
+});
+
+productsBtn.addEventListener('click', () => {
+  removeActive();
+  productsBtn.classList.add('active-link');
+  productsContent.classList.remove('hidden');
+  homeContent.classList.add('hidden');
+});
+
+homeBtn.classList.add('active-link');
+homeContent.classList.remove('hidden');
+productsContent.classList.add('hidden');
